0fbb152495d18d3ddee69c28eb3c8205b7ec5a3a
Ahmed-Magdi-Prog <ahmedmagdimohamed72@gmail.com>
2020-12-30 14:38:20 +0200

    first commit

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fast_Market.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult cart()
+        {
+            return View();
+        }
+
+        public IActionResult details()
+        {
+            return View();
+        }
+
+        public IActionResult slideBar()
+        {
+            return View();
+        }
+        public IActionResult wishList()
+        {
+            return View();
+        }
+    }
+}
